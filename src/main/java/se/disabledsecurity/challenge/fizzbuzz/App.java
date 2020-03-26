1dbf9f01b15a19619e55daa809d4350a829144ab
Mattias Carlsson <mattias.carlsson01@gmail.com>
2020-03-26 17:51:28 +0100

    Initial commit

---
+++ src/main/java/se/disabledsecurity/challenge/fizzbuzz/App.java
@@ -0,0 +1,14 @@
+package se.disabledsecurity.challenge.fizzbuzz;
+
+import java.util.stream.IntStream;
+
+public class App {
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(x -> x % 15 == 0 ? "FizzBuzz" : x % 5 == 0 ? "Buzz" : x % 3 == 0 ? "Fizz" : x)
+                .forEach(System.out::println);
+
+
+    }
+
+}
