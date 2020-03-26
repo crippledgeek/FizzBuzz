82398b2d5266eb07b711b180eba998a268f7e5c8
Mattias Carlsson <mattias.carlsson01@gmail.com>
2020-03-26 22:14:23 +0100

    Added test skeleton

--- src/main/java/se/disabledsecurity/challenge/fizzbuzz/App.java
+++ src/main/java/se/disabledsecurity/challenge/fizzbuzz/App.java
@@ -3,7 +3,9 @@ package se.disabledsecurity.challenge.fizzbuzz;
 import java.util.stream.IntStream;
 
 public class App {
+
     public static void main(String[] args) {
+
         IntStream.rangeClosed(1, 100)
                 .mapToObj(x -> x % 15 == 0 ? "FizzBuzz" : x % 5 == 0 ? "Buzz" : x % 3 == 0 ? "Fizz" : x)
                 .forEach(System.out::println);
